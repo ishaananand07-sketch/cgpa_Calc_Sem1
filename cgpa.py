8531cee172ab9142b3865fce809644e214123f6b
ishaananand07-sketch <ishaananand07@gmail.com>
2026-01-07 17:29:29 +0530

    Change default values for number inputs in cgpa.py
    Updated default values for number inputs in CGPA calculation.

--- cgpa.py
+++ cgpa.py
@@ -22,10 +22,10 @@ if group == 1:
 
     # LAC
     st.markdown("### Linear Algebra and Calculus (LAC)")
-    ese = st.number_input("ESE (60)", 0.0, 60.0, 1.0, key="g1_lac_ese")
-    ise = st.number_input("ISE (20)", 0.0, 20.0, 1.0, key="g1_lac_ise")
-    cie = st.number_input("CIE (20)", 0.0, 20.0, 1.0, key="g1_lac_cie")
-    tw  = st.number_input("TW (60)", 0.0, 60.0, 1.0, key="g1_lac_tw")
+    ese = st.number_input("ESE (60)", 0.0, 60.0, 0.0, 1.0, key="g1_lac_ese")
+    ise = st.number_input("ISE (20)", 0.0, 20.0, 0.0, 1.0, key="g1_lac_ise")
+    cie = st.number_input("CIE (20)", 0.0, 20.0, 0.0, 1.0, key="g1_lac_cie")
+    tw  = st.number_input("TW (60)", 0.0, 60.0, 0.0, 1.0, key="g1_lac_tw")
     total = ese + ise + cie + (tw / 60 * 25)
     perc = total / 125 * 100
     subjects.append(("LAC", 4, perc, gp(perc)))
@@ -33,67 +33,67 @@ if group == 1:
     # QP
     st.markdown("### Quantum Physics (QP)")
     qp = (
-        st.number_input("ESE (60)", 0.0, 60.0, 1.0, key="g1_qp_ese") +
-        st.number_input("ISE (20)", 0.0, 20.0, 1.0, key="g1_qp_ise") +
-        st.number_input("CIE (20)", 0.0, 20.0, 1.0, key="g1_qp_cie")
+        st.number_input("ESE (60)", 0.0, 60.0, 0.0, 1.0, key="g1_qp_ese") +
+        st.number_input("ISE (20)", 0.0, 20.0, 0.0, 1.0, key="g1_qp_ise") +
+        st.number_input("CIE (20)", 0.0, 20.0, 0.0, 1.0, key="g1_qp_cie")
     )
     subjects.append(("QP", 2, qp, gp(qp)))
 
     # QPL
     st.markdown("### Quantum Physics Lab (QPL)")
-    qpl = st.number_input("TW (120)", 0.0, 120.0, 1.0, key="g1_qpl_tw")
+    qpl = st.number_input("TW (120)", 0.0, 120.0, 0.0, 1.0, key="g1_qpl_tw")
     subjects.append(("QPL", 1, qpl / 120 * 100, gp(qpl / 120 * 100)))
 
     # MFR
     st.markdown("### Mechanics for Robotics (MFR)")
     mfr = (
-        st.number_input("ESE (60)", 0.0, 60.0, 1.0, key="g1_mfr_ese") +
-        st.number_input("ISE (20)", 0.0, 20.0, 1.0, key="g1_mfr_ise") +
-        st.number_input("CIE (20)", 0.0, 20.0, 1.0, key="g1_mfr_cie")
+        st.number_input("ESE (60)", 0.0, 60.0, 0.0, 1.0, key="g1_mfr_ese") +
+        st.number_input("ISE (20)", 0.0, 20.0, 0.0, 1.0, key="g1_mfr_ise") +
+        st.number_input("CIE (20)", 0.0, 20.0, 0.0, 1.0, key="g1_mfr_cie")
     )
     subjects.append(("MFR", 2, mfr, gp(mfr)))
 
     # MFRL
     st.markdown("### Mechanics for Robotics Lab (MFRL)")
-    mfrl = st.number_input("TW (110)", 0.0, 110.0, 1.0, key="g1_mfrl_tw")
+    mfrl = st.number_input("TW (110)", 0.0, 110.0, 0.0, 1.0, key="g1_mfrl_tw")
     subjects.append(("MFRL", 1, mfrl / 110 * 100, gp(mfrl / 110 * 100)))
 
     # IEEE
     st.markdown("### Integrated Electrical and Electronics Engineering (IEEE)")
     ieee = (
-        st.number_input("ESE (60)", 0.0, 60.0, 1.0, key="g1_ieee_ese") +
-        st.number_input("ISE (20)", 0.0, 20.0, 1.0, key="g1_ieee_ise") +
-        st.number_input("CIE (20)", 0.0, 20.0, 1.0, key="g1_ieee_cie")
+        st.number_input("ESE (60)", 0.0, 60.0, 0.0, 1.0, key="g1_ieee_ese") +
+        st.number_input("ISE (20)", 0.0, 20.0, 0.0, 1.0, key="g1_ieee_ise") +
+        st.number_input("CIE (20)", 0.0, 20.0, 0.0, 1.0, key="g1_ieee_cie")
     )
     subjects.append(("IEEE", 2, ieee, gp(ieee)))
 
     # IEEEL
     st.markdown("### Integrated Electrical and Electronics Engineering Lab (IEEEL)")
-    ieeel = st.number_input("TW (120)", 0.0, 120.0, 1.0, key="g1_ieeel_tw")
+    ieeel = st.number_input("TW (120)", 0.0, 120.0, 0.0, 1.0, key="g1_ieeel_tw")
     subjects.append(("IEEEL", 1, ieeel / 120 * 100, gp(ieeel / 120 * 100)))
 
     # CPPS
     st.markdown("### C Programming for Problem Solving (CPPS)")
     cpps = (
-        st.number_input("ESE (60)", 0.0, 60.0, 1.0, key="g1_cpps_ese") +
-        st.number_input("ISE (20)", 0.0, 20.0, 1.0, key="g1_cpps_ise") +
-        st.number_input("CIE (20)", 0.0, 20.0, 1.0, key="g1_cpps_cie")
+        st.number_input("ESE (60)", 0.0, 60.0, 0.0, 1.0, key="g1_cpps_ese") +
+        st.number_input("ISE (20)", 0.0, 20.0, 0.0, 1.0, key="g1_cpps_ise") +
+        st.number_input("CIE (20)", 0.0, 20.0, 0.0, 1.0, key="g1_cpps_cie")
     )
     subjects.append(("CPPS", 2, cpps, gp(cpps)))
 
     # CPPSL
     st.markdown("### C Programming for Problem Solving Lab (CPPSL)")
-    cppsl = st.number_input("TW (100)", 0.0, 100.0, 1.0, key="g1_cppsl_tw")
+    cppsl = st.number_input("TW (100)", 0.0, 100.0, 0.0, 1.0, key="g1_cppsl_tw")
     subjects.append(("CPPSL", 1, cppsl, gp(cppsl)))
 
     # FAB
     st.markdown("### FAB Lab (FL)")
-    fab = st.number_input("TW (50)", 0.0, 50.0, 1.0, key="g1_fab_tw")
+    fab = st.number_input("TW (50)", 0.0, 50.0, 0.0, 1.0, key="g1_fab_tw")
     subjects.append(("FL", 1, fab / 50 * 100, gp(fab / 50 * 100)))
 
     # IKS
     st.markdown("### Indian Knowledge System (IKS)")
-    iks = st.number_input("TW (30)", 0.0, 30.0, 1.0, key="g1_iks_tw")
+    iks = st.number_input("TW (30)", 0.0, 30.0, 0.0, 1.0, key="g1_iks_tw")
     subjects.append(("IKS", 2, iks / 30 * 100, gp(iks / 30 * 100)))
 
 # ======================= GROUP 2 =======================
@@ -102,10 +102,10 @@ elif group == 2:
 
     # LAC
     st.markdown("### Linear Algebra and Calculus (LAC)")
-    ese = st.number_input("ESE (60)", 0.0, 60.0, 1.0, key="g2_lac_ese")
-    ise = st.number_input("ISE (20)", 0.0, 20.0, 1.0, key="g2_lac_ise")
-    cie = st.number_input("CIE (20)", 0.0, 20.0, 1.0, key="g2_lac_cie")
-    tw  = st.number_input("TW (60)", 0.0, 60.0, 1.0, key="g2_lac_tw")
+    ese = st.number_input("ESE (60)", 0.0, 60.0, 0.0, 1.0, key="g2_lac_ese")
+    ise = st.number_input("ISE (20)", 0.0, 20.0, 0.0, 1.0, key="g2_lac_ise")
+    cie = st.number_input("CIE (20)", 0.0, 20.0, 0.0, 1.0, key="g2_lac_cie")
+    tw  = st.number_input("TW (60)", 0.0, 60.0, 0.0, 1.0, key="g2_lac_tw")
     total = ese + ise + cie + (tw / 60 * 25)
     perc = total / 125 * 100
     subjects.append(("LAC", 4, perc, gp(perc)))
@@ -113,69 +113,69 @@ elif group == 2:
     # CST
     st.markdown("### Chemical Science and Technology (CST)")
     cst = (
-        st.number_input("ESE (60)", 0.0, 60.0, 1.0, key="g2_cst_ese") +
-        st.number_input("ISE (20)", 0.0, 20.0, 1.0, key="g2_cst_ise") +
-        st.number_input("CIE (20)", 0.0, 20.0, 1.0, key="g2_cst_cie")
+        st.number_input("ESE (60)", 0.0, 60.0, 0.0, 1.0, key="g2_cst_ese") +
+        st.number_input("ISE (20)", 0.0, 20.0, 0.0, 1.0, key="g2_cst_ise") +
+        st.number_input("CIE (20)", 0.0, 20.0, 0.0, 1.0, key="g2_cst_cie")
     )
     subjects.append(("CST", 2, cst, gp(cst)))
 
     # CSTL
     st.markdown("### Chemical Science and Technology Lab (CSTL)")
-    cstl = st.number_input("TW (100)", 0.0, 100.0, 1.0, key="g2_cstl_tw")
+    cstl = st.number_input("TW (100)", 0.0, 100.0, 0.0, 1.0, key="g2_cstl_tw")
     subjects.append(("CSTL", 1, cstl, gp(cstl)))
 
     # CGD
     st.markdown("### Computer Graphics and Design (CGD)")
     cgd = (
-        st.number_input("ESE (60)", 0.0, 60.0, 1.0, key="g2_cgd_ese") +
-        st.number_input("ISE (20)", 0.0, 20.0, 1.0, key="g2_cgd_ise") +
-        st.number_input("CIE (20)", 0.0, 20.0, 1.0, key="g2_cgd_cie")
+        st.number_input("ESE (60)", 0.0, 60.0, 0.0, 1.0, key="g2_cgd_ese") +
+        st.number_input("ISE (20)", 0.0, 20.0, 0.0, 1.0, key="g2_cgd_ise") +
+        st.number_input("CIE (20)", 0.0, 20.0, 0.0, 1.0, key="g2_cgd_cie")
     )
     subjects.append(("CGD", 2, cgd, gp(cgd)))
 
     # CGDL
     st.markdown("### Computer Graphics and Design Lab (CGDL)")
-    cgdl = st.number_input("TW (70)", 0.0, 70.0, 1.0, key="g2_cgdl_tw")
+    cgdl = st.number_input("TW (70)", 0.0, 70.0, 0.0, 1.0, key="g2_cgdl_tw")
     subjects.append(("CGDL", 1, cgdl / 70 * 100, gp(cgdl / 70 * 100)))
 
     # CPPS
     st.markdown("### C Programming for Problem Solving (CPPS)")
     cpps = (
-        st.number_input("ESE (60)", 0.0, 60.0, 1.0, key="g2_cpps_ese") +
-        st.number_input("ISE (20)", 0.0, 20.0, 1.0, key="g2_cpps_ise") +
-        st.number_input("CIE (20)", 0.0, 20.0, 1.0, key="g2_cpps_cie")
+        st.number_input("ESE (60)", 0.0, 60.0, 0.0, 1.0, key="g2_cpps_ese") +
+        st.number_input("ISE (20)", 0.0, 20.0, 0.0, 1.0, key="g2_cpps_ise") +
+        st.number_input("CIE (20)", 0.0, 20.0, 0.0, 1.0, key="g2_cpps_cie")
     )
     subjects.append(("CPPS", 2, cpps, gp(cpps)))
 
     # CPPSL
     st.markdown("### C Programming for Problem Solving Lab (CPPSL)")
-    cppsl = st.number_input("TW (100)", 0.0, 100.0, 1.0, key="g2_cppsl_tw")
+    cppsl = st.number_input("TW (100)", 0.0, 100.0, 0.0, 1.0, key="g2_cppsl_tw")
     subjects.append(("CPPSL", 1, cppsl, gp(cppsl)))
 
     # IIDTL
     st.markdown("### Innovative Idea and Design Thinking Lab (IIDTL)")
-    iidtl = st.number_input("TW (60)", 0.0, 60.0, 1.0, key="g2_iidtl_tw")
+    iidtl = st.number_input("TW (60)", 0.0, 60.0, 0.0, 1.0, key="g2_iidtl_tw")
     subjects.append(("IIDTL", 1, iidtl / 60 * 100, gp(iidtl / 60 * 100)))
 
     # ESE
     st.markdown("### Environment and Sustainable Engineering (ESE)")
     ese2 = (
-        st.number_input("ESE (60)", 0.0, 60.0, 1.0, key="g2_ese_ese") +
-        st.number_input("ISE (20)", 0.0, 20.0, 1.0, key="g2_ese_ise") +
-        st.number_input("CIE (20)", 0.0, 20.0, 1.0, key="g2_ese_cie")
+        st.number_input("ESE (60)", 0.0, 60.0, 0.0, 1.0, key="g2_ese_ese") +
+        st.number_input("ISE (20)", 0.0, 20.0, 0.0, 1.0, key="g2_ese_ise") +
+        st.number_input("CIE (20)", 0.0, 20.0, 0.0, 1.0, key="g2_ese_cie")
     )
     subjects.append(("ESE", 2, ese2, gp(ese2)))
 
     # SS
     st.markdown("### Soft Skills (SS)")
-    ss_tw = st.number_input("TW (40)", 0.0, 40.0, 1.0, key="g2_ss_tw")
-    ss_cie = st.number_input("CIE (25)", 0.0, 25.0, 1.0, key="g2_ss_cie")
+    ss_tw = st.number_input("TW (40)", 0.0, 40.0, 0.0, 1.0, key="g2_ss_tw")
+    ss_cie = st.number_input("CIE (25)", 0.0, 25.0, 0.0, 1.0, key="g2_ss_cie")
     ss_total = (ss_tw / 40 * 25) + ss_cie
     subjects.append(("SS", 2, ss_total / 50 * 100, gp(ss_total / 50 * 100)))
 
     # CCA
     st.markdown("### Cocurricular Activity-1 (CCA-1)")
-    cca = st.number_input("TW (25)", 0.0, 25.0, 1.0, key="g2_cca_tw")
+    cca = st.number_input("TW (25)", 0.0, 25.0, 0.0, 1.0, key="g2_cca_tw")
     subjects.append(("CCA-1", 1, cca / 25 * 100, gp(cca / 25 * 100)))
 
 # ================= RESULT =================
